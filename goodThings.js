c513f648a9771130403931e0bcdfe291c3730f76
noamrap <128160855+noamrap@users.noreply.github.com>
2023-03-23 21:48:18 +0200

    hx

---
+++ goodThings.js
@@ -0,0 +1,15 @@
+﻿function openModal(img) {
+    var modal = document.getElementById("myModal");
+    var modalImg = document.getElementById("img01");
+    modal.style.display = "block";
+    modalImg.src = img.src;
+    var span = document.getElementsByClassName("close")[0];
+    span.onclick = function () {
+        modal.style.display = "none";
+    }
+    window.onclick = function (event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+}
